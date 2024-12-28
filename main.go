4b11f47ffa8c98de81ae916007240bf7467febdf
abrakingoo <maikoabraham68@gmail.com>
2024-12-28 12:06:16 +0300

    update: adds more routes

--- main.go
+++ main.go
@@ -1,16 +1,34 @@
 package main
 
 import (
+	"ecomerce/db"
 	"ecomerce/handlers"
 	"log"
 	"net/http"
 )
 
+func init() {
+	if err := db.InitDB(); err != nil {
+		log.Fatal(err.Error())
+	}
+
+	if err := db.CreateTables(); err != nil {
+		log.Fatal(err.Error())
+	}
+
+}
 
 func main() {
-
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.HandleFunc("/", handlers.HomeHandler)
+	http.HandleFunc("/signin", handlers.SigninHandler)
+	http.HandleFunc("/signout", handlers.SignOutHandler)
+	http.HandleFunc("/signup", handlers.SignupHandler)
+	http.HandleFunc("/error", handlers.ErrorHandler)
+	http.HandleFunc("/cart", handlers.CartHandler)
+	http.HandleFunc("/profile", handlers.ProfileHandler)
+	http.HandleFunc("/remove", handlers.RemoveHandler)
+	http.HandleFunc("/product", handlers.ProductsHandler)
 	log.Println("Server running on https://localhost:8080")
 	http.ListenAndServe(":8080", nil)
-} 
+}
